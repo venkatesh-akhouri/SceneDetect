c39214eacca9013d07d5118fc422cf4cc8c2da40
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-07-24 00:28:02 +0530

    initial data analysis

---
+++ scripts/data_analysis.py
@@ -0,0 +1,30 @@
+import os
+from collections import Counter
+
+class_counts=Counter()
+script_dir = os.path.dirname(os.path.abspath(__file__)) # .../scenedetect/scripts
+project_root = os.path.dirname(script_dir)
+
+labels_path=os.path.join(project_root, 'data', 'kitti', 'data_object_image_2', 'training_labels', 'label_2')
+
+if os.path.exists(labels_path):
+    print("exists")
+    for filename in os.listdir(labels_path):
+        if filename.endswith('.txt'):
+            #read file
+            with open (os.path.join(labels_path, filename), 'r') as file:
+                words=file.read().strip().split()
+                if words:
+                    class_name=words[0]
+                    class_counts[class_name] += 1
+                    
+
+print("-"*50)
+print("class_Counts")
+print("-"*50)
+
+for cls, count in class_counts.items():
+        print(f"{cls:<15}: {count}")
+
+            
+    
