555c0a2ec8c476fde8acf90ee5e955266d22f37b
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-08-14 01:13:00 +0200

    feat: script to generate yaml file for YOLO

---
+++ scripts/generate_yaml.py
@@ -0,0 +1,34 @@
+import yaml
+import os
+
+
+script_dir=os.path.dirname(os.path.abspath(__file__))
+root_dir=os.path.dirname(script_dir)
+images_dir=os.path.join(root_dir, "data","kitti", "data_object_image_2","training","images")
+
+yaml_path=os.path.join(root_dir, "training","configs")
+
+#check if config folder exixts
+if not os.path.exists(yaml_path):
+    os.makedirs(yaml_path)
+
+
+class_map={"Car": 0,
+           "Pedestrian": 1,
+           "Cyclist": 2,
+           "Van": 3,
+           "Truck": 4}
+
+
+yaml_dict={
+    "path":images_dir,
+    "train":"train",
+    "val":"val",
+    "names":{value:key for key,value in class_map.items()}
+}
+
+#generate the yaml file
+yaml_file=f"{yaml_path}/data.yaml"
+with open(yaml_file, "w") as f:
+    yaml.dump(yaml_dict, f)
+    
